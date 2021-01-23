6effabf8e2caeec1d4ee851ba1a8760f7c6d79cf
hanaf <hanaf.othmane@gmail.com>
2021-01-23 15:04:38 -0500

    update on click

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,21 +3,30 @@ import React, { useState } from "react";
 function App() {
 
   const [name, setName] = useState("");
+  const [headingText, setheadingText] = useState(name)
 
   function handleChange(event){
     setName(event.target.value);
   }
 
+  function submitName(event){
+    setheadingText(name);
+
+    event.preventDefault();
+  }
+
   return (
     <div className="container">
-      <h1>Hello {name}</h1>
-      <input 
-        onChange={handleChange} 
-        type="text" 
-        placeholder="What's your name?" 
-        value = {name}
-      />
-      <button>Submit</button>
+      <h1>Hello {headingText}</h1>
+      <form onSubmit={submitName}>
+        <input 
+          onChange={handleChange} 
+          type="text"
+          placeholder="What's your name?" 
+          value = {name}
+        />
+        <button type="submit">Submit</button>
+      </form>
     </div>
   );
 }
